b785c13e192cd081f37371e992a0b51fea1a7113
Victor Anogwih <anogwihvictor@gmail.com>
2023-12-13 02:12:40 -0500

    Completed Task

--- client.js
+++ client.js
@@ -1,20 +1,38 @@
 const net = require("net");
 
-// establishes a connection with the game server
+// Establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
     host: "localhost", // IP address here,
     port: 50541,
   });
 
-  // interpret incoming data as text
+  // Interpret incoming data as text
   conn.setEncoding("utf8");
+
+  // Event handler for incoming data
   conn.on('data', (data) => {
-    console.log('Server msg: ', data)
+    console.log('Server msg:', data);
   });
+
+  // Event handler for successful connection
   conn.on('connect', () => {
-    conn.write("Successfully connected to game server");
-    conn.write("Name: VIC")
+    // Commands with different delays
+    const commands = [
+      { command: "Successfully connected to game server", delay: 50 },
+      { command: "Name: VIC", delay: 100 },
+      { command: "Move: up", delay: 200 },
+      { command: "Move: down", delay: 300 },
+      { command: "Move: left", delay: 400 },
+      { command: "Move: right", delay: 500 },
+    ];
+
+    // Loop through commands and set timeouts
+    commands.forEach(({ command, delay }) => {
+      setInterval(() => {
+        conn.write(command);
+      }, delay);
+    });
   });
 
   return conn;
